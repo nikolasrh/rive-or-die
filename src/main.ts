f09bd60c64eb49b5a5cd642943d20944e6e86085
Hanna A. Aalmen <hanna.aalmen@gmail.com>
2024-10-23 16:40:32 +0200

    Endrer logoer

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+import logo from '/rive_or_die_logo.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+      <img src="${logo}" class="logo" alt="Logo" />
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
