6ebc81d52f373abcc9379a68b770da07a9dde98d
Hanna A. Aalmen <hanna.aalmen@gmail.com>
2024-12-06 10:14:41 +0100

    Adding insert

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,60 @@
-import './style.css'
-import { createRiveElement } from './create-rive-element'
+import "./style.css";
+import { createRiveElement } from "./create-rive-element";
+import { supabase } from "./supabase";
 
-const dropzone = document.querySelector<HTMLDivElement>('#dropzone')!
-const dropzoneInput = document.querySelector<HTMLInputElement>('#dropzone-input')!
+const dropzone = document.querySelector<HTMLDivElement>("#dropzone")!;
+const dropzoneInput =
+  document.querySelector<HTMLInputElement>("#dropzone-input")!;
 
-let inputX = 0
-let inputY = 0
+let inputX = 0;
+let inputY = 0;
 
 dropzone.ondblclick = (event) => {
-  inputX = event.clientX
-  inputY = event.clientY
-  dropzoneInput.click()
-}
+  inputX = event.clientX;
+  inputY = event.clientY;
+  dropzoneInput.click();
+};
 
 dropzoneInput.onchange = () => {
-  uploadFiles(dropzoneInput.files, inputX, inputY)
-}
+  uploadFiles(dropzoneInput.files, inputX, inputY);
+};
 
 dropzone.ondragover = (event) => {
-  event.preventDefault()
-}
+  event.preventDefault();
+};
 
 dropzone.ondrop = (event) => {
-  event.preventDefault()
-  event.stopPropagation()
-  uploadFiles(event.dataTransfer?.files, event.clientX, event.clientY)
-}
+  event.preventDefault();
+  event.stopPropagation();
+  uploadFiles(event.dataTransfer?.files, event.clientX, event.clientY);
+};
 
-const uploadFiles = (files: FileList | undefined | null, x: number, y: number) => {
-  Array.from(files || [])
-    .map(file => createRiveElement(file, x, y))
-    .forEach(newElement => {
-      // Remove element on double click
-      newElement.ondblclick = event => {
-        event.stopPropagation()
-        dropzone.removeChild(newElement)
-      }
+const uploadFiles = (
+  files: FileList | undefined | null,
+  x: number,
+  y: number
+) => {
+  Array.from(files || []).forEach(async (newElement) => {
+    const { error } = await supabase
+      .from("animations")
+      .insert({ pos_x: x, pos_y: y, data: newElement });
 
-      dropzone.appendChild(newElement)
-    })
-}
+    if (error) {
+      console.error("Error uploading new animation!");
+    }
+  });
+};
 
+// Remove element on double click
+/*
+  newElement.ondblclick = (event) => {
+    event.stopPropagation();
+    dropzone.removeChild(newElement);
+  };
+
+      .map((file) => createRiveElement(file, x, y))
+
+  if (data) {
+    console.log(data);
+    dropzone.appendChild(newElement);
+  }*/
--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,8 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
-const url = 'https://lcdclunxclyztgmvladr.supabase.co'
-const publicKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjZGNsdW54Y2x5enRnbXZsYWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0NzQ3MDMsImV4cCI6MjA0OTA1MDcwM30.pfXW1WeqqOyvY08uKlc18W_gOKMoz8yaMIdnIw34gL8'
-const client = createClient(url, publicKey)
+const url = "https://lcdclunxclyztgmvladr.supabase.co";
+const publicKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjZGNsdW54Y2x5enRnbXZsYWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0NzQ3MDMsImV4cCI6MjA0OTA1MDcwM30.pfXW1WeqqOyvY08uKlc18W_gOKMoz8yaMIdnIw34gL8";
+const client = createClient(url, publicKey);
 
-export default client
+export default client;
