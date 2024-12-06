77394f0abc681b1813220dd68541b82e9a877f4c
Nikolas Rølland Hugsted <13964858+nikolasrh@users.noreply.github.com>
2024-12-06 09:51:02 +0100

    Add supabase

---
+++ src/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const url = 'https://lcdclunxclyztgmvladr.supabase.co'
+const publicKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjZGNsdW54Y2x5enRnbXZsYWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0NzQ3MDMsImV4cCI6MjA0OTA1MDcwM30.pfXW1WeqqOyvY08uKlc18W_gOKMoz8yaMIdnIw34gL8'
+const supabase = createClient(url, publicKey)
